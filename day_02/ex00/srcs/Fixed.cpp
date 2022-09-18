7825808857abc3d37d309ceb4942f34f2386720a
agiraudet <agiraude@e1r9p3.clusters.42paris.fr>
2022-09-18 17:39:54 +0200

    wip

--- day_02/ex00/srcs/Fixed.cpp
+++ day_02/ex00/srcs/Fixed.cpp
@@ -6,30 +6,35 @@
 /*   By: agiraude <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 16:57:44 by agiraude          #+#    #+#             */
-/*   Updated: 2022/09/18 17:09:58 by agiraude         ###   ########.fr       */
+/*   Updated: 2022/09/18 17:39:32 by agiraude         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
+#include <iostream>
 
 Fixed::Fixed(void)
 : _n(0)
 {
+	std::cout << "Default constructor called" << std::endl;
 	return ;
 }
 
 Fixed::Fixed(Fixed const & src)
 {
+	std::cout << "Copy constructor called" << std::endl;
 	this = *src;
 }
 
 Fixed::~Fixed(void)
 {
+	std::cout << "Destructor called" << std::endl;
 	return ;
 }
 
 Fixed &	Fixed::operator=(Fixed const & rhs)
 {
+	std::cout << "
 	this.setRawBits(rhs.getRawBits());
 	return *this;
 }
