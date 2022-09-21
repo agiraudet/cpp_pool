cf71b6a863fc00c139766e4f15a3368c145f9a3b
agiraudet <agiraude@e1r10p6.clusters.42paris.fr>
2022-09-21 15:04:16 +0200

    wip day05

---
+++ day_05/ex00/srcs/main.cpp
@@ -0,0 +1,91 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: agiraude <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 14:46:02 by agiraude          #+#    #+#             */
+/*   Updated: 2022/09/21 15:02:12 by agiraude         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+int	main()
+{
+	std::cout << "# Simple bureaucrat" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	a("Tom", 28);
+			std::cout << a << std::endl;
+			a.incGrade();
+			std::cout << a << std::endl;
+			a.decGrade();
+			std::cout << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "# The BEST bureaucrat" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	a("Tom", 0);
+			std::cout << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "# The (almost) BEST bureaucrat" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	a("Tom", 1);
+			std::cout << a << std::endl;
+			a.incGrade();
+			std::cout << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "# The WORST bureaucrat" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	a("Tom", 151);
+			std::cout << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "# The (almost) WORST bureaucrat" << std::endl;
+	{
+		try
+		{
+			Bureaucrat	a("Tom", 150);
+			std::cout << a << std::endl;
+			a.decGrade();
+			std::cout << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << "error: " << e.what() << std::endl;
+		}
+	}
+	std::cout << std::endl;
+	return (0);
+}
